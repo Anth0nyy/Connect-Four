0ebc5ceec00a6259b308ce05539bc0ebc87b07aa
Anthony <anthony.ant.zhao@gmail.com>
2018-01-01 14:43:19 -0500

    Idk

---
+++ ConnectFour/src/connectfour/GameBoard.java
@@ -0,0 +1,10 @@
+package connectfour;
+
+public class GameBoard {
+	
+	
+	public final int ROW = 7;
+	public final int COL = 6;
+	public int [][] grid = new int[ROW][COL];
+	
+}
---
+++ ConnectFour/src/connectfour/Logic.java
@@ -0,0 +1,14 @@
+package connectfour;
+
+public class Logic extends GameBoard {
+	
+	
+	
+	public boolean winner(boolean firstPlayerTurn, int row, int col) {
+		return true;
+	}
+	
+	public int row(int col) {
+		return 1;
+	}
+}
